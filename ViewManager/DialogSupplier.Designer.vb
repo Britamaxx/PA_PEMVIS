f6cb5f5155e75d1a197e0f499aa1abaf819c0e3f
EuphoriaBot <dimasfirja80@gmail.com>
2026-05-20 11:10:28 +0800

    DialogSupplier

--- ViewManager/DialogSupplier.Designer.vb
+++ ViewManager/DialogSupplier.Designer.vb
@@ -22,101 +22,101 @@ Partial Class DialogSupplier
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        TextBox1 = New TextBox()
+        txtSupplierID = New TextBox()
         Label1 = New Label()
-        TextBox2 = New TextBox()
-        MaskedTextBox1 = New MaskedTextBox()
-        TextBox4 = New TextBox()
-        Button1 = New Button()
-        Button2 = New Button()
+        txtSupplierName = New TextBox()
+        txtSupplierPhone = New MaskedTextBox()
+        txtSupplierAddress = New TextBox()
+        btnKonfirmasi = New Button()
+        btnBatal = New Button()
         Label2 = New Label()
         Label3 = New Label()
         Label5 = New Label()
         SuspendLayout()
         ' 
-        ' TextBox1
+        ' txtSupplierID
         ' 
-        TextBox1.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        TextBox1.Location = New Point(157, 10)
-        TextBox1.Margin = New Padding(2, 2, 2, 2)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(217, 30)
-        TextBox1.TabIndex = 3
+        txtSupplierID.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtSupplierID.Location = New Point(196, 12)
+        txtSupplierID.Margin = New Padding(2)
+        txtSupplierID.Name = "txtSupplierID"
+        txtSupplierID.Size = New Size(270, 34)
+        txtSupplierID.TabIndex = 3
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
         Label1.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label1.Location = New Point(10, 12)
+        Label1.Location = New Point(12, 15)
         Label1.Margin = New Padding(2, 0, 2, 0)
         Label1.Name = "Label1"
-        Label1.Size = New Size(94, 23)
+        Label1.Size = New Size(109, 28)
         Label1.TabIndex = 4
         Label1.Text = "Supplier ID"
         ' 
-        ' TextBox2
+        ' txtSupplierName
         ' 
-        TextBox2.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        TextBox2.Location = New Point(157, 42)
-        TextBox2.Margin = New Padding(2, 2, 2, 2)
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(217, 30)
-        TextBox2.TabIndex = 5
+        txtSupplierName.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtSupplierName.Location = New Point(196, 52)
+        txtSupplierName.Margin = New Padding(2)
+        txtSupplierName.Name = "txtSupplierName"
+        txtSupplierName.Size = New Size(270, 34)
+        txtSupplierName.TabIndex = 5
         ' 
-        ' MaskedTextBox1
+        ' txtSupplierPhone
         ' 
-        MaskedTextBox1.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        MaskedTextBox1.Location = New Point(157, 74)
-        MaskedTextBox1.Margin = New Padding(2, 2, 2, 2)
-        MaskedTextBox1.Mask = "____-____-____"
-        MaskedTextBox1.Name = "MaskedTextBox1"
-        MaskedTextBox1.Size = New Size(217, 30)
-        MaskedTextBox1.TabIndex = 8
+        txtSupplierPhone.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtSupplierPhone.Location = New Point(196, 92)
+        txtSupplierPhone.Margin = New Padding(2)
+        txtSupplierPhone.Mask = "____-____-____"
+        txtSupplierPhone.Name = "txtSupplierPhone"
+        txtSupplierPhone.Size = New Size(270, 34)
+        txtSupplierPhone.TabIndex = 8
         ' 
-        ' TextBox4
+        ' txtSupplierAddress
         ' 
-        TextBox4.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        TextBox4.Location = New Point(157, 108)
-        TextBox4.Margin = New Padding(2, 2, 2, 2)
-        TextBox4.Multiline = True
-        TextBox4.Name = "TextBox4"
-        TextBox4.Size = New Size(217, 97)
-        TextBox4.TabIndex = 10
+        txtSupplierAddress.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtSupplierAddress.Location = New Point(196, 135)
+        txtSupplierAddress.Margin = New Padding(2)
+        txtSupplierAddress.Multiline = True
+        txtSupplierAddress.Name = "txtSupplierAddress"
+        txtSupplierAddress.Size = New Size(270, 120)
+        txtSupplierAddress.TabIndex = 10
         ' 
-        ' Button1
+        ' btnKonfirmasi
         ' 
-        Button1.BackColor = Color.LimeGreen
-        Button1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Button1.ForeColor = Color.White
-        Button1.Location = New Point(270, 209)
-        Button1.Margin = New Padding(2, 2, 2, 2)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(104, 27)
-        Button1.TabIndex = 21
-        Button1.Text = "Konfirmasi"
-        Button1.UseVisualStyleBackColor = False
+        btnKonfirmasi.BackColor = Color.LimeGreen
+        btnKonfirmasi.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnKonfirmasi.ForeColor = Color.White
+        btnKonfirmasi.Location = New Point(338, 261)
+        btnKonfirmasi.Margin = New Padding(2)
+        btnKonfirmasi.Name = "btnKonfirmasi"
+        btnKonfirmasi.Size = New Size(130, 34)
+        btnKonfirmasi.TabIndex = 21
+        btnKonfirmasi.Text = "Konfirmasi"
+        btnKonfirmasi.UseVisualStyleBackColor = False
         ' 
-        ' Button2
+        ' btnBatal
         ' 
-        Button2.BackColor = Color.Red
-        Button2.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Button2.ForeColor = Color.White
-        Button2.Location = New Point(158, 209)
-        Button2.Margin = New Padding(2, 2, 2, 2)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(104, 27)
-        Button2.TabIndex = 22
-        Button2.Text = "Batal"
-        Button2.UseVisualStyleBackColor = False
+        btnBatal.BackColor = Color.Red
+        btnBatal.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnBatal.ForeColor = Color.White
+        btnBatal.Location = New Point(198, 261)
+        btnBatal.Margin = New Padding(2)
+        btnBatal.Name = "btnBatal"
+        btnBatal.Size = New Size(130, 34)
+        btnBatal.TabIndex = 22
+        btnBatal.Text = "Batal"
+        btnBatal.UseVisualStyleBackColor = False
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
         Label2.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label2.Location = New Point(10, 44)
+        Label2.Location = New Point(12, 55)
         Label2.Margin = New Padding(2, 0, 2, 0)
         Label2.Name = "Label2"
-        Label2.Size = New Size(123, 23)
+        Label2.Size = New Size(142, 28)
         Label2.TabIndex = 23
         Label2.Text = "Supplier Name"
         ' 
@@ -124,10 +124,10 @@ Partial Class DialogSupplier
         ' 
         Label3.AutoSize = True
         Label3.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label3.Location = New Point(10, 76)
+        Label3.Location = New Point(12, 95)
         Label3.Margin = New Padding(2, 0, 2, 0)
         Label3.Name = "Label3"
-        Label3.Size = New Size(126, 23)
+        Label3.Size = New Size(145, 28)
         Label3.TabIndex = 24
         Label3.Text = "Supplier Phone"
         ' 
@@ -135,42 +135,42 @@ Partial Class DialogSupplier
         ' 
         Label5.AutoSize = True
         Label5.Font = New Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label5.Location = New Point(10, 110)
+        Label5.Location = New Point(12, 138)
         Label5.Margin = New Padding(2, 0, 2, 0)
         Label5.Name = "Label5"
-        Label5.Size = New Size(137, 23)
+        Label5.Size = New Size(160, 28)
         Label5.TabIndex = 26
         Label5.Text = "Supplier Address"
         ' 
         ' DialogSupplier
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(382, 245)
+        ClientSize = New Size(478, 306)
         Controls.Add(Label5)
         Controls.Add(Label3)
         Controls.Add(Label2)
-        Controls.Add(Button2)
-        Controls.Add(Button1)
-        Controls.Add(TextBox4)
-        Controls.Add(MaskedTextBox1)
-        Controls.Add(TextBox2)
+        Controls.Add(btnBatal)
+        Controls.Add(btnKonfirmasi)
+        Controls.Add(txtSupplierAddress)
+        Controls.Add(txtSupplierPhone)
+        Controls.Add(txtSupplierName)
         Controls.Add(Label1)
-        Controls.Add(TextBox1)
-        Margin = New Padding(2, 2, 2, 2)
+        Controls.Add(txtSupplierID)
+        Margin = New Padding(2)
         Name = "DialogSupplier"
         Text = "Form8"
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtSupplierID As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents MaskedTextBox1 As MaskedTextBox
-    Friend WithEvents TextBox4 As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents txtSupplierName As TextBox
+    Friend WithEvents txtSupplierPhone As MaskedTextBox
+    Friend WithEvents txtSupplierAddress As TextBox
+    Friend WithEvents btnKonfirmasi As Button
+    Friend WithEvents btnBatal As Button
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label5 As Label
--- ViewManager/DialogSupplier.vb
+++ ViewManager/DialogSupplier.vb
@@ -1,3 +1,68 @@
 ﻿Public Class DialogSupplier
 
+    Public Mode As String = "Tambah"
+    Public IDTerpilih As String = ""
+
+    Private Sub DialogSupplier_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Mode = "Edit" Then
+            TampilDataEdit()
+        Else
+            txtSupplierID.Enabled = True
+        End If
+    End Sub
+
+    Private Sub TampilDataEdit()
+        Dim dt As DataTable = GetSupplierByID(IDTerpilih)
+        If dt.Rows.Count = 0 Then
+            MessageBox.Show("Data tidak ditemukan", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Me.Close()
+            Exit Sub
+        End If
+
+        Dim row As DataRow = dt.Rows(0)
+        txtSupplierID.Text = row("id_supplier").ToString()
+        txtSupplierID.Enabled = False
+        txtSupplierName.Text = row("nama_supplier").ToString()
+        txtSupplierPhone.Text = row("telepon_supplier").ToString()
+        txtSupplierAddress.Text = row("alamat").ToString()
+    End Sub
+
+    Private Sub btnBatal_Click(sender As Object, e As EventArgs) Handles btnBatal.Click
+        Me.DialogResult = DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub btnKonfirmasi_Click(sender As Object, e As EventArgs) Handles btnKonfirmasi.Click
+        If txtSupplierID.Text.Trim() = "" OrElse txtSupplierName.Text.Trim() = "" OrElse
+           txtSupplierPhone.Text.Trim() = "" OrElse txtSupplierAddress.Text.Trim() = "" Then
+            MessageBox.Show("Semua field harus diisi", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        Dim id As String = txtSupplierID.Text.Trim()
+        Dim nama As String = txtSupplierName.Text.Trim()
+        Dim telepon As String = txtSupplierPhone.Text.Trim()
+        Dim alamat As String = txtSupplierAddress.Text.Trim()
+
+        If Mode = "Tambah" Then
+            If SimpanSupplier(id, nama, alamat, telepon) Then
+                MessageBox.Show("Data supplier berhasil disimpan", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Me.DialogResult = DialogResult.OK
+                Me.Close()
+            End If
+        Else
+            If UbahSupplier(id, nama, alamat, telepon) Then
+                MessageBox.Show("Data supplier berhasil diubah", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Me.DialogResult = DialogResult.OK
+                Me.Close()
+            End If
+        End If
+    End Sub
+
+    Private Sub txtSupplierPhone_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtSupplierPhone.KeyPress
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
 End Class
